78e39e7248ca134550daf4a305e51faf3c0bc546
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-09-16 21:28:51 +0100

    viewport test instance

--- cypress/e2e/my-own-tests/hello-world.cy.js
+++ cypress/e2e/my-own-tests/hello-world.cy.js
@@ -8,4 +8,11 @@ describe('Basic Test', () => {
 
     cy.contains('Explore All Roadmaps').click();
   });
+
+  it('Viewport size', () => {
+    cy.viewport(320, 568);
+    cy.visit('https://codedamn.com');
+    cy.contains('Learn Programming').should('be.visible');
+    cy.contains('Explore All Roadmaps').click();
+  });
 });
