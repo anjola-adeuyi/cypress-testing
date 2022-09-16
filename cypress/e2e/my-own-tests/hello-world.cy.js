efbb89b30e7a3724dc760d75bc11ae5b9acd2211
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-09-16 21:22:25 +0100

    cypress assertion

--- cypress/e2e/my-own-tests/hello-world.cy.js
+++ cypress/e2e/my-own-tests/hello-world.cy.js
@@ -5,5 +5,7 @@ describe('Basic Test', () => {
     cy.visit('https://codedamn.com');
 
     cy.contains('Learn Programming').should('be.visible');
+
+    cy.contains('Explore All Roadmaps').click();
   });
 });
